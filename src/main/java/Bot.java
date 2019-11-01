be33c91e6a6a4a3289d209c5b63fad5a1dcbf8e4
Maksym <maksym.kozachuk@protonmail.com>
2019-11-01 20:12:13 +0100

    parse + sending

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,13 +1,22 @@
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+
 
 public class Bot extends TelegramLongPollingBot {
 
     private long chat_id;
+    private Movie movie = new Movie();
 
     public void onUpdateReceived(Update update) {
         update.getUpdateId();
@@ -37,6 +46,37 @@ public class Bot extends TelegramLongPollingBot {
         if (msg.contains("Hi") || msg.contains("Cześć") || msg.contains("Hello")) {
             return "Hello!";
         }
+        if (msg.contains("info")) {
+            return getInfoMovie();
+        }
         return msg;
+    }
+
+    public String getInfoMovie() {
+        SendPhoto sendPhoto = new SendPhoto();
+
+
+        try (InputStream in = new URL(movie.getImage()).openStream()) {
+
+            Files.copy(in, Paths.get("/Users/maksymkozachuk/Desktop/t/1.jpg"));
+            sendPhoto.setChatId(chat_id);
+            sendPhoto.setPhoto(new File("/Users/maksymkozachuk/Desktop/t/1.jpg"));
+            execute(sendPhoto);
+            Files.delete(Paths.get("/Users/maksymkozachuk/Desktop/t/1.jpg"));
+
+        } catch (IOException e) {
+            System.out.println("Image File Not Found");
+        } catch (TelegramApiException e) {
+            System.out.println("Exception when execute sendPhoto");
+        }
+
+        String info = movie.getTitle()
+                + "\n" + movie.getGenres()
+                + "\n Description " + movie.getDescription()
+                + "\n Score " + movie.getScore();
+
+
+        return info;
+
     }
 }
---
+++ src/main/java/Movie.java
@@ -0,0 +1,49 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Movie {
+    private Document document;
+
+    public Movie() {
+        connect();
+    }
+
+    private void connect() {
+        try {
+            document = Jsoup.connect("https://www.metacritic.com/movie/marriage-story").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("IOE in Jsoup connect");
+        }
+    }
+
+    public String getTitle() {
+        return document.title();
+    }
+
+    public String getScore() {
+        Elements elements = document.getElementsByClass("metascore_w larger movie positive");
+        return elements.text();
+    }
+
+    public String getDescription() {
+        Elements elements = document.getElementsByClass("summary_deck details_section");
+        return elements.text();
+    }
+
+    public String getGenres() {
+        Elements elements = document.getElementsByClass("genres");
+        return elements.text();
+    }
+
+    public String getImage() {
+        Elements elements = document.getElementsByClass("summary_img");
+        String url = elements.attr("src");
+        return url;
+
+    }
+}
